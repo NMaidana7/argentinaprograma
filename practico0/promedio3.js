e61a85e6f4862358e43e3a10ae94b3907960bc98
Nicolás Maidana <nicomaidana7@live.com>
2023-05-25 00:32:15 -0300

    Practico N0 finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,21 @@
+
+
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+
+let num11 = -2;
+let num22 = -3;
+let num33 = -8;
+
+let num111 = -1.2;
+let num222 = 5.3;
+let num333 = -3.3;
+
+let promedio1 = (num1 + num2 + num3) / 3;
+let promedio2 = (num11 + num22 + num33) / 3;
+let promedio3 = (num111 + num222 + num333) / 3;
+
+console.log(promedio1);
+console.log(promedio2);
+console.log(promedio3);
