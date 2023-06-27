0c74578cc778850e6fb153ecfb9bb9618f001169
Nicolás Maidana <nicomaidana7@live.com>
2023-06-27 14:56:57 -0300

    Agragando carpeta y lanzamiento

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,11 @@
+
+
+for (let cohete=10; cohete >= 0; cohete --) {
+      if (cohete == 10) {
+    console.log("Cuenta regresiva 10");
+  } else if (cohete == 0) {
+    console.log("Lanzamiento!");
+  } else {
+    console.log(cohete);
+  }
+}
